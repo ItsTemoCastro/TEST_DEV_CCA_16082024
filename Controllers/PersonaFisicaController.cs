d33052a4a9c712da9f3c16a1e10d8dcd4b783a61
ItsTemoCastro <78826149+ItsTemoCastro@users.noreply.github.com>
2024-08-18 01:21:29 -0600

    INIT PROYECT-> configuracion inicial de los endpoints y la clase del DTO

---
+++ Controllers/PersonaFisicaController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TEST_DEV_CCA_14082024.Dtos;
+
+namespace TEST_DEV_CCA_14082024.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonaFisicaController : Controller
+    {
+        [HttpGet]
+        public async Task<PersonaFisicaDto> GetAllPersonasFisica()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<PersonaFisicaDto>GetPersonaFisica(int IdPersonaFisica)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<bool> DeletePersonaFisica(int IdPersonaFisica)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<PersonaFisicaDto> AddPersonaFisica(AddPersonaFisicaDto personaFisica)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public async Task<bool> UpdatePersonaFisica(PersonaFisicaDto personaFisica)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dtos/AddPersonaFisicaDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TEST_DEV_CCA_14082024.Dtos
+{
+    public class AddPersonaFisicaDto
+    {
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El apellido paterno es requerido")]
+        public string ApellidoPaterno { get; set; }
+
+        [Required(ErrorMessage = "El apellido materno es requerido")]
+        public string ApellidoMaterno { get; set; }
+
+        [Required(ErrorMessage = "El RFC es requerido")]
+        public string RFC { get; set; }
+
+        [Required(ErrorMessage = "La fecha de nacimiento es requerida")]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "El usuario que modifica el registro es necesario")]
+        public string UsuarioAgrega { get; set; }
+
+    }
+}
